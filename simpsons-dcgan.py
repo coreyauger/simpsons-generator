bd31e2f84cd268139d7ae3023e8b79061d8e0329
Corey Auger <coreyauger@gmail.com>
2018-06-10 00:14:03 -0700

    dcgan

---
+++ simpsons-dcgan.py
@@ -0,0 +1,308 @@
+import glob
+import os
+
+
+import numpy as np
+from sklearn.utils import shuffle
+from scipy import misc
+from PIL import Image
+import pandas as pd 
+import matplotlib
+from matplotlib import pyplot as plt
+import tensorflow as tf
+#matplotlib.interactive(True)
+
+random_seed = 90210
+np.random.seed(random_seed)
+
+img_w, img_h = 64, 64
+img_channels = 3
+
+def crop_center(file, img,cropx,cropy):
+    print(file)
+    y,x,c = img.shape
+    if(x < 64 or y < 64):
+        os.remove(file)
+    startx = x//2-(cropx//2)
+    starty = y//2-(cropy//2)    
+    return img[starty:starty+cropy,startx:startx+cropx]
+
+path = '/home/suroot/Documents/data/simpsons/data/'
+outpath = '/home/suroot/Documents/data/simpsons/data128/'
+
+
+def clean(path, size):
+    allFiles = glob.glob(os.path.join(path, "*/*.jpg"))   
+    ind = 0 
+    for infile in allFiles:
+        try:
+            im = Image.open(infile).convert('RGB')
+            savepath = outpath + str( ind ) + ".png"
+            ind += 1
+            print(savepath)
+            print(im.size)
+            im.thumbnail( (size, size), Image.ANTIALIAS)
+            im.save(savepath, "PNG")
+        except IOError:
+            print("cannot create thumbnail for " + savepath)
+
+#clean(path, 128)
+
+def create_nparray_from_data(path, subset = -1):
+    allFiles = glob.glob(os.path.join(path, "*.png"))    
+    if(subset > 0):
+        allFiles = allFiles[0:subset]
+    data = []
+    # NOTE: set np.random.seed for reproducability
+    shuffleFiles = shuffle(sorted(allFiles))
+    img_test = shuffleFiles[ int(np.random.random() * len(shuffleFiles)) ] 
+    print("random file: " + img_test)
+    x = misc.imread(img_test)
+    x = x/255
+    x = crop_center(img_test, x,img_w,img_h)
+    print(x.shape)
+    plt.imshow( x )
+    plt.show()
+    data = np.array( [crop_center(x, misc.imread(x), img_w, img_h) for x in shuffleFiles] )
+    data = data/255
+    print(data.shape)
+    return data
+
+#data = create_nparray_from_data(outpath)
+#print(data.shape)
+
+#np.save("./data/data.npy", data)
+#data = np.load("./data/data-small.npy")
+data = np.load("./data/data.npy")
+print(data.shape)
+print("DONE DONE DONE")
+
+#img_test = data[ int(np.random.random() * data.shape[0]),:,:,: ] 
+#plt.imshow(img_test)
+#plt.show()
+
+
+idx = np.random.randint(0, data.shape[0], size=36)
+fig, axes = plt.subplots(6, 6, sharex=True, sharey=True, figsize=(5,5),)
+for ii, ax in zip(idx, axes.flatten()):
+    ax.imshow(data[ii,:,:,:], aspect='equal')
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.show()
+
+input("Press Enter to start training.")
+
+
+###################################################################################
+##  Model
+###################################################################################
+
+def model_inputs(real_dim, z_dim):
+    inputs_real = tf.placeholder(tf.float32, (None, *real_dim), name='input_real')
+    inputs_z = tf.placeholder(tf.float32, (None, z_dim), name='input_z')
+    return inputs_real, inputs_z
+
+def generator(z, output_dim, reuse=False, alpha=0.2, training=True):
+    with tf.variable_scope('generator', reuse=reuse):
+        # First fully connected layer
+        x1 = tf.layers.dense(z, 4*4*1024)
+        # Reshape it to start the convolutional stack
+        x1 = tf.reshape(x1, (-1, 4, 4, 1024))
+        x1 = tf.layers.batch_normalization(x1, training=training)
+        x1 = tf.maximum(alpha * x1, x1)
+        # 4x4x1024 now
+        
+        x2 = tf.layers.conv2d_transpose(x1, 512, 5, strides=2, padding='same')
+        x2 = tf.layers.batch_normalization(x2, training=training)
+        x2 = tf.maximum(alpha * x2, x2)
+        # 8x8x512 now
+        
+        x3 = tf.layers.conv2d_transpose(x2, 256, 5, strides=2, padding='same')
+        x3 = tf.layers.batch_normalization(x3, training=training)
+        x3 = tf.maximum(alpha * x3, x3)
+        # 16x16x256 now
+
+        x4 = tf.layers.conv2d_transpose(x3, 128, 5, strides=2, padding='same')
+        x4 = tf.layers.batch_normalization(x4, training=training)
+        x4 = tf.maximum(alpha * x4, x4)
+        # 32x32x128 now
+        
+        # Output layer
+        logits = tf.layers.conv2d_transpose(x3, output_dim, 5, strides=2, padding='same')
+        # 64x64x3 now
+        
+        out = tf.tanh(logits)
+        
+        return out
+
+
+
+def  discriminator(x, reuse=False, alpha=0.2):
+    with tf.variable_scope('discriminator', reuse=reuse):
+        # Input layer is 64x64x3
+        x1 = tf.layers.conv2d(x, 128, 5, strides=2, padding='same')
+        relu1 = tf.maximum(alpha * x1, x1)
+        # 16x16x64
+        
+        x2 = tf.layers.conv2d(relu1, 256, 5, strides=2, padding='same')
+        bn2 = tf.layers.batch_normalization(x2, training=True)
+        relu2 = tf.maximum(alpha * bn2, bn2)
+        # 8x8x256
+        
+        x3 = tf.layers.conv2d(relu2, 512, 5, strides=2, padding='same')
+        bn3 = tf.layers.batch_normalization(x3, training=True)
+        relu3 = tf.maximum(alpha * bn3, bn3)
+        # 4x4x512
+
+        # Flatten it
+        flat = tf.reshape(relu3, (-1, 4*4*512))
+        logits = tf.layers.dense(flat, 1)
+        out = tf.sigmoid(logits)
+        
+        return out, logits
+
+
+def model_loss(input_real, input_z, output_dim, alpha=0.2):
+    """
+    Get the loss for the discriminator and generator
+    :param input_real: Images from the real dataset
+    :param input_z: Z input
+    :param out_channel_dim: The number of channels in the output image
+    :return: A tuple of (discriminator loss, generator loss)
+    """
+    g_model = generator(input_z, output_dim, alpha=alpha)
+    d_model_real, d_logits_real = discriminator(input_real, alpha=alpha)
+    d_model_fake, d_logits_fake = discriminator(g_model, reuse=True, alpha=alpha)
+
+    d_loss_real = tf.reduce_mean(
+        tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logits_real, labels=tf.ones_like(d_model_real)))
+    d_loss_fake = tf.reduce_mean(
+        tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logits_fake, labels=tf.zeros_like(d_model_fake)))
+    g_loss = tf.reduce_mean(
+        tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logits_fake, labels=tf.ones_like(d_model_fake)))
+
+    d_loss = d_loss_real + d_loss_fake
+
+    return d_loss, g_loss
+
+
+
+def model_opt(d_loss, g_loss, learning_rate, beta1):
+    """
+    Get optimization operations
+    :param d_loss: Discriminator loss Tensor
+    :param g_loss: Generator loss Tensor
+    :param learning_rate: Learning Rate Placeholder
+    :param beta1: The exponential decay rate for the 1st moment in the optimizer
+    :return: A tuple of (discriminator training operation, generator training operation)
+    """
+    # Get weights and bias to update
+    t_vars = tf.trainable_variables()
+    d_vars = [var for var in t_vars if var.name.startswith('discriminator')]
+    g_vars = [var for var in t_vars if var.name.startswith('generator')]
+
+    # Optimize
+    with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+        d_train_opt = tf.train.AdamOptimizer(learning_rate, beta1=beta1).minimize(d_loss, var_list=d_vars)
+        g_train_opt = tf.train.AdamOptimizer(learning_rate, beta1=beta1).minimize(g_loss, var_list=g_vars)
+
+    return d_train_opt, g_train_opt
+
+
+class GAN:
+    def __init__(self, real_size, z_size, learning_rate, alpha=0.2, beta1=0.5):
+        tf.reset_default_graph()
+        
+        self.input_real, self.input_z = model_inputs(real_size, z_size)
+        
+        self.d_loss, self.g_loss = model_loss(self.input_real, self.input_z,
+                                              real_size[2], alpha=alpha)
+        
+        self.d_opt, self.g_opt = model_opt(self.d_loss, self.g_loss, learning_rate, beta1)
+
+
+def view_samples(epoch, samples, nrows, ncols, figsize=(5,5)):
+    fig, axes = plt.subplots(figsize=figsize, nrows=nrows, ncols=ncols, 
+                             sharey=True, sharex=True)
+    for ax, img in zip(axes.flatten(), samples[epoch]):
+        ax.axis('off')
+        img = ((img - img.min())*255 / (img.max() - img.min())).astype(np.uint8)
+        ax.set_adjustable('box-forced')
+        im = ax.imshow(img, aspect='equal')
+   
+    plt.subplots_adjust(wspace=0, hspace=0)
+    return fig, axes
+
+
+
+def train(net, dataset, epochs, batch_size, print_every=10, show_every=100, figsize=(5,5)):
+    saver = tf.train.Saver()
+    sample_z = np.random.uniform(-1, 1, size=(72, z_size))
+
+    samples, losses = [], []
+    steps = 0
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for e in range(epochs):
+            for x, y in dataset.batches(batch_size):
+                steps += 1
+
+                # Sample random noise for G
+                batch_z = np.random.uniform(-1, 1, size=(batch_size, z_size))
+
+                # Run optimizers
+                _ = sess.run(net.d_opt, feed_dict={net.input_real: x, net.input_z: batch_z})
+                _ = sess.run(net.g_opt, feed_dict={net.input_z: batch_z, net.input_real: x})
+
+                if steps % print_every == 0:
+                    # At the end of each epoch, get the losses and print them out
+                    train_loss_d = net.d_loss.eval({net.input_z: batch_z, net.input_real: x})
+                    train_loss_g = net.g_loss.eval({net.input_z: batch_z})
+
+                    print("Epoch {}/{}...".format(e+1, epochs),
+                          "Discriminator Loss: {:.4f}...".format(train_loss_d),
+                          "Generator Loss: {:.4f}".format(train_loss_g))
+                    # Save losses to view after training
+                    losses.append((train_loss_d, train_loss_g))
+
+                if steps % show_every == 0:
+                    gen_samples = sess.run(
+                                   generator(net.input_z, 3, reuse=True, training=False),
+                                   feed_dict={net.input_z: sample_z})
+                    samples.append(gen_samples)
+                    _ = view_samples(-1, samples, 6, 12, figsize=figsize)
+                    plt.show()
+
+        saver.save(sess, './checkpoints/generator.ckpt')
+
+    with open('samples.pkl', 'wb') as f:
+        pkl.dump(samples, f)
+    
+    return losses, samples
+
+
+real_size = (64,64,3)
+z_size = 100
+learning_rate = 0.0002
+batch_size = 128
+epochs = 25
+alpha = 0.2
+beta1 = 0.5
+
+# Create the network
+net = GAN(real_size, z_size, learning_rate, alpha=alpha, beta1=beta1)
+
+
+dataset = Dataset(trainset, testset)
+
+losses, samples = train(net, dataset, epochs, batch_size, figsize=(10,5))
+
+
+fig, ax = plt.subplots()
+losses = np.array(losses)
+plt.plot(losses.T[0], label='Discriminator', alpha=0.5)
+plt.plot(losses.T[1], label='Generator', alpha=0.5)
+plt.title("Training Losses")
+plt.legend()
